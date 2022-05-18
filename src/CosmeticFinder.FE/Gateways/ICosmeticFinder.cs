08beebc40a5e3dfef37458231dc63c54f6623e36
Weronika Kubica <w.kubicaweronika@gmail.com>
2022-05-18 18:23:55 +0200

    [ADD] Add "GetCosmeticRequest" class and rebuild GET

--- src/CosmeticFinder.FE/Gateways/ICosmeticFinder.cs
+++ src/CosmeticFinder.FE/Gateways/ICosmeticFinder.cs
@@ -1,12 +1,13 @@
-﻿using CosmeticFinder.FE.Gateways.Responses;
+﻿using CosmeticFinder.FE.Gateways.Requests;
+using CosmeticFinder.FE.Gateways.Responses;
 using Refit;
 
 namespace CosmeticFinder.FE.Gateways
 {
     public interface ICosmeticFinder
     {
-        [Get("/Cosmetics?search={search}&mainCategoryId={mainCategoryId}&sort={sort}&sortByPriceAsc={sortByPriceAsc}&shouldContainCompose={shouldContainCompose}")]
-        Task<IEnumerable<CosmeticResponse>> GetCosmetic(string search, int mainCategoryId, bool sort, bool sortByPriceAsc, bool shouldContainCompose);
+        [Get("/Cosmetics?search={request.search}&mainCategoryId={request.mainCategoryId}&sort={request.sort}&sortByPriceAsc={request.sortByPriceAsc}&shouldContainCompose={request.shouldContainCompose}")]
+        Task<IEnumerable<CosmeticResponse>> GetCosmetic(GetCosmeticsRequest request);
 
         [Get("/Categories")]
         Task<IEnumerable<CategoryResponse>> GetCategories();
---
+++ src/CosmeticFinder.FE/Gateways/Requests/GetCosmeticsRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmeticFinder.FE.Gateways.Requests;
+
+public class GetCosmeticsRequest
+{
+    public string search { get; set; }
+    public int mainCategoryId { get; set; }
+    public bool sort { get; set; }
+    public bool sortByPriceAsc { get; set; }
+    public bool shouldContainCompose { get; set; }
+}
