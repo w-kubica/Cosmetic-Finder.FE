2538c0477431c84f09c06c26161b74e909c03972
Weronika Kubica <w.kubicaweronika@gmail.com>
2022-10-23 19:16:06 +0200

    Add picture

--- src/CosmeticFinder.FE/Gateways/Responses/CosmeticResponse.cs
+++ src/CosmeticFinder.FE/Gateways/Responses/CosmeticResponse.cs
@@ -10,6 +10,7 @@
         public string Compose { get; set; }
         public double Price { get; set; }
         public int MainCategoryId { get; set; }
+        public string Picture { get; set; }
     }
 
 }
